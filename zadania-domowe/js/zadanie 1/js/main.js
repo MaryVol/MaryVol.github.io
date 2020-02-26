27468ab6f4fdc50d2c7682bf64d4237cc7bc690e
MaryVol <meryswhite@icloud.com>
2020-02-26 13:44:22 +0100

    final

--- zadania-domowe/js/zadanie 1/js/main.js
+++ zadania-domowe/js/zadanie 1/js/main.js
@@ -1,22 +1,23 @@
-// table=[1, 2, 3, 4, 5, 6]
-//     for (let i = 0; i < table.length; i++) {
-//         let result 
-//         result+= this
-//         console.log('Suma jest rowna', result) 
-//     }  
-
-//     table.forEach(element => {
-//         let result=element+(element+1)
-
-//         console.log('suma jest rowna', result)
-//     });
 
 table=[1, 2, 3, 4, 5, 6]
-    for (let i = 0; i < table.length; i++) {
-        let result
-        result=+table[i]
-        console.log("TCL: result", result)
-        
-    }  
+
+function table() {
+let suma=table[0]
+for (let i = 1; i < table.length; i++) {
+    suma=suma+ table[i]
+}  
+console.log("TCL: suma", suma)
+
+let iloczyn=table[0]
+for (let i = 0; i < table.length; i++) {
+
+    iloczyn=iloczyn * table[i]
+    return iloczyn
+}  
+console.log("TCL: iloczyn", iloczyn)
+}
+
+
+
 
 
