b00545f2ff6deed64eb55b3aa1fed3b1157c4881
MaryVol <meryswhite@icloud.com>
2020-03-01 11:33:27 +0100

    ajax warsztaty

--- AJAX/js/3-jquery-ajax.js
+++ AJAX/js/3-jquery-ajax.js
@@ -1,44 +1,59 @@
-$("#btnGetData").click(function () {
-	let id = $("#number").val();
-	console.log(id)
+let number=Math.floor(Math.random()*10)
 
-
-	if (id <= 9) {
-		$.getJSON(
-			`https://jsonplaceholder.typicode.com/users/${id}`,
-			function (data) {
-
-				// wyswietl w konsoli
-				console.log(data);
-			}
-		)
-	} else {
-		alert("błąd ")
-	}
+fetch(`https://jsonplaceholder.typicode.com/users/${number}`)
+.then(res=> res.json())
+.then(res=>{
+	console.log(res)
+	let elem= document.getElementById('resName')
+	elem.appendChild("Have a good day, " + res.username)
+	
+}).catch(err => {
+	console.log(err)
 })
 
 
 
+// $("#btnGetData").click(function () {
+// 	let id = $("#number").val();
+// 	console.log(id)
 
-// $.getJSON(
-// 	`https://jsonplaceholder.typicode.com/users/${id}`,
-// 	function (data) {
 
-// 		// wyswietl w konsoli
-// 		console.log(data);
+// 	if (id <= 9) {
+// 		$.getJSON(
+// 			`https://jsonplaceholder.typicode.com/users/${id}`,
+// 			function (data) {
+
+// 				// wyswietl w konsoli
+// 				console.log(data);
+// 			}
+// 		)
+// 	} else {
+// 		alert("błąd ")
 // 	}
-// );
+// })
 
 
 
-function dodawanie(a, b) {
 
-	console.log(a + b)
-	1
-}
+// // $.getJSON(
+// // 	`https://jsonplaceholder.typicode.com/users/${id}`,
+// // 	function (data) {
+
+// // 		// wyswietl w konsoli
+// // 		console.log(data);
+// // 	}
+// // );
 
 
-dodawanie(4, 6);
-dodawanie(4, 6);
+
+// function dodawanie(a, b) {
+
+// 	console.log(a + b)
+// 	1
+// }
+
+
+// dodawanie(4, 6);
+// dodawanie(4, 6);
 
 
