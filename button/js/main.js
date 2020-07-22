b2c4b947dbb301e0e3162e4d3c3badacfa1825cd
MaryVol <meryswhite@icloud.com>
2020-07-22 14:24:24 +0200

    recruitment

--- button/js/main.js
+++ button/js/main.js
@@ -1,2 +1,26 @@
 "use strict";
 
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", rich);
+function rich() {
+  btn.innerHTML = "Congratulations, you are rich";
+}
+
+btn.addEventListener("dblclick", famous);
+function famous() {
+  btn.innerHTML = "Congratulations, you are famous";
+}
+
+btn.addEventListener("mouseleave", reset);
+function reset() {
+  btn.innerHTML = "Well, decide";
+}
+
+addEventListener("keypress", background);
+function background() {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+  document.body.style.transition = "0.5s";
+  document.body.style.transitionDelay = "0.5s";
+}
